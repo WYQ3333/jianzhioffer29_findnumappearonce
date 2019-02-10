f853682a327e69113fc49e96bd1c3b3736c84613
浅忆梦微凉 <3538922590@qq.com>
2019-02-10 20:08:57 +0800

    大神代码

--- 剑指offer29_数组中出现一次的数字/test.cpp
+++ 剑指offer29_数组中出现一次的数字/test.cpp
@@ -34,7 +34,7 @@ public:
 	}
 };
 
-class Solution {
+class Solution2 {
 public:
 	void FindNumsAppearOnce(vector<int> data, int* num1, int *num2) {
 		int i = 0;
@@ -56,6 +56,70 @@ public:
 			}
 		}
 	}
+};
+
+class Solution3 {
+public:
+	void FindNumsAppearOnce(vector<int> data, int* num1, int *num2) {
+		int i = 0;
+		int j = 0;
+		int count = 0;
+		for (i = 0; i < data.size(); ++i){
+			for (j = 0; j < data.size(); ++j){
+				if (j != i&&data[i] == data[j]){
+					break;
+				}
+			}
+			if (j == data.size() && count == 0){
+				num1[0] = data[i];
+				count++;
+			}
+			else if (j == data.size() && count == 1){
+				num2[0] = data[i];
+				count++;
+			}
+		}
+	}
+};
+
+class Solution {
+public:
+	void FindNumsAppearOnce(vector<int> data, int* num1, int* num2) {
+		if (data.size() < 2)
+			return;
+
+		int resultExclusiveOR = 0;
+		for (int i = 0; i < data.size(); i++){
+			resultExclusiveOR ^= data[i];
+		}
+
+		unsigned int indexOf1 = FindFirstBitIs1(resultExclusiveOR);
+
+		*num1 = *num2 = 0;
+		for (int j = 0; j < data.size(); j++){
+			if (IsBit1(data[j], indexOf1))
+				*num1 ^= data[j];
+			else
+				*num2 ^= data[j];
+		}
+
+	}
+
+	unsigned int FindFirstBitIs1(int num){
+		int indexBit = 0;
+		while (((num & 1) == 0) && (indexBit < 8 * sizeof(int))){
+			num = num >> 1;
+			indexBit++;
+		}
+
+		return indexBit;
+	}
+
+	bool IsBit1(int num, unsigned int indexBit){
+		num = num >> indexBit;
+		return (num & 1);
+	}
+
 };
 
 void TestFunc(){
